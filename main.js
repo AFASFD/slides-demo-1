6eaa91696a6e2aad31c1144ad330098b3cfeb821
FrankYang <1025414646@qq.com>
2018-04-15 18:01:05 +0800

    111

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 let imgsLength, n
 
-init()
-slidePlay(3000)
+init() 
+slidePlay(3000)    
+
+
+
+
+
+
+
 
 
 function init() {
     n = 1
     imgLength = $('.images > img').length
     $('.images > img:nth-child(1)').addClass('current').siblings().addClass('enter')
-
 }
-
 function slidePlay(time) {
     setInterval(() => {
         makeLeave(getImage(n)).one('transitionend', (e) => {
@@ -20,11 +25,9 @@ function slidePlay(time) {
         n += 1
     }, time)
 }
-
 function getImage(n) {
     return $(`.images > img:nth-child(${x(n)})`)
 }
-
 function makeCurrent($node) {
     return $node.removeClass('enter leave').addClass('current')
 }
